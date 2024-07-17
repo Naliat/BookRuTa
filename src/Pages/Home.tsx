f82864e70f4b42f69a1eaae608828ac298b33ac2
Naliat <tailansouza@alu.ufc.br>
2024-07-16 21:31:08 -0300

    Alterações

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -4,7 +4,10 @@ import './Home.css';
 const Home: React.FC = () => {
   return (
     <div className="home-container">
-      {/* <h1 className="home-title">Bem-vindo ao Sistema de Gestão de Livros</h1> */}
+      <h1 className="home-title">Bem-vindo ao Sistema de Gestão de Livros</h1>
+      <p className="home-description">
+        O site foi desenvolvido com foco em proporcionar uma experiência intuitiva e funcional para os usuários. Ele inclui funcionalidades como cadastro de livros, atualização de informações, login de usuários e registro de novos usuários. A página inicial apresenta um menu de navegação facilitado por um dropdown responsivo, garantindo acessibilidade em diferentes dispositivos. O objetivo principal é oferecer uma plataforma eficiente para gestão e interação com informações relacionadas a livros, visando simplicidade e eficácia na utilização.
+      </p>
     </div>
   );
 };
